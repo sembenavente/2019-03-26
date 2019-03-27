10f283ce5ee6880564a6733e5c0db3aee22768bd
sembenavente <sembenavente@upeu.edu.pe>
2019-03-26 19:32:54 -0500

    hola

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
-var nombre = prompt("Ingresar Nombre");
-alert("Bienvenido: " + nombre);
-console.log(nombre);
+alert("hola");
+
+
+for (let index = 0; index < 1; index++) {
+
+    var cantHoras = prompt("Ingresar las horas laborales");
+    var cant = Number.parseFloat(cantHoras);
+
+    if(!(cant<0 || cant >60)){
+        
+        calcularSueldo(cant);
+    } else {
+        alert("Error: ingreso una cantidad de horas no valido");
+        index = -1
+    }
+    
+}
+
+function calcularSueldo(cant){
+    var sueldo = 0;
+    var pagoHora = 40;
+if (cant <= 40) {
+    sueldo = cant*pagoHora;
+    alert("el sueldo total es de: " + sueldo);
+} else {
+    if (cant <= 50) {
+        sueldo = ((cant-40) * (pagoHora * 2) + ((cant - (cant-40))*pagoHora));
+        alert("el sueldo total es de: " + sueldo);
+    } else {
+        var horasEx = 10;
+        var horSupExtra = cant - 50;
+        sueldo = (40 * pagoHora) + (horasEx * (pagoHora*2)) + (horSupExtra * (pagoHora*3));
+        alert("el sueldo total es de: " + sueldo);
+    }
+}
+}
